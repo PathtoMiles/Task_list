67fb34425f376c28c01649623bf23a915cabf4df
PathtoMiles <aze.rodriguez.m@gmail.com>
2025-01-23 17:36:01 -0600

    Desactivar DEBUG y limpiar configuración

--- src/proyecto/proyecto/settings.py
+++ src/proyecto/proyecto/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['task-list-vz7o.onrender.com', '127.0.0.1']
 
 
 # Application definition
